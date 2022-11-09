6b1b48e83e7fe281416b2d8790763e0206efda96
SaravananBalki26 <117564913+SaravananBalki26@users.noreply.github.com>
2022-11-09 11:28:20 +0530

    Add files via upload

---
+++ week1day1/Mobile.java
@@ -0,0 +1,28 @@
+package week1day1;
+
+public class Mobile {
+
+	String mobileBrandName="Oneplus";
+	char mobileLogo='1';
+	short noOfMobilePiece=31000;
+	int modelNumber=54;
+	long mobileImeiNumber=210000L;
+	float mobilePrice=400.32f;
+	boolean isDamageStrid=true;
+		
+	public static void main(String[] args)
+	{
+		Mobile tech=new Mobile();
+		System.out.println(tech.mobileBrandName);
+		System.out.println(tech.mobileLogo);
+		System.out.println(tech.noOfMobilePiece);
+		System.out.println(tech.mobileImeiNumber);
+		System.out.println(tech.mobilePrice);
+		System.out.println(tech.modelNumber);
+		System.out.println(tech.isDamageStrid);
+		
+	}
+	
+
+}
+	
---
+++ week1day1/TwoWheeler.java
@@ -0,0 +1,26 @@
+package week1day1;
+
+public class TwoWheeler {
+		int noOfWheels=100;
+		short noOfMirrors=200;
+		long chasisNumber=214000L;
+		boolean isPunctured=true;
+		String bikeName="Yamaha R15";
+		double runningKM=60000.68;
+
+	public static void main(String[] args)
+	{
+		TwoWheeler auto= new TwoWheeler();
+		System.out.println(auto.noOfWheels);
+		System.out.println(auto.noOfMirrors);
+		System.out.println(auto.chasisNumber);
+		System.out.println(auto.isPunctured);
+		System.out.println(auto.bikeName);
+		System.out.println(auto.runningKM);
+		
+	}
+
+	}
+
+
+
